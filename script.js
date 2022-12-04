6460eab6f7068cd3bfaf56bcf1b4afb0652ee4d9
Dash NZITA <117472865+Dash-dev-5@users.noreply.github.com>
2022-12-04 11:08:59 +0100

    ...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function citat(){
-    let citations=[["Dash nzita","Vivre sans risque c'est mourire sans histoire"],["Sele Shabani","La meilleur de formation se fait seul"],["Tony nimi","Si le css etais une fille donc l'htmml serai l'homme"],["Meddy","Le js est comme le foot plus tu y joue plus tu prend gout sur tous avec react"],["Grace Ruhana","La beauté d'un site se resumer par sa maquette"],["Tiatania","Si le coeur de l'homme etais un systeme informatique je serais le premier a hacker celle de mon pere"],["cyclope","Si le css n'existais pas la beauté n'aura pas de sence"],["Maluta","Le web et rien que le web"]];
+    let citations=[["Dash nzita","Vivre sans risque c'est mourir sans histoire"],["Sele Shabani","La meilleur de formation se fait seul"],["Tony nimi","Si le css etais une fille donc l'htmml serai l'homme"],["Meddy","Le js est comme le foot plus tu y joue plus tu prend gout sur tous avec react"],["Grace Ruhana","La beauté d'un site se resumer par sa maquette"],["Tiatania","Si le coeur de l'homme etais un systeme informatique je serais le premier a hacker celle de mon pere"],["cyclope","Si le css n'existais pas la beauté n'aura pas de sence"],["Maluta","Le web et rien que le web"]];
     let num=Math.floor(Math.random()*(8-0)+0) ;
     let auteur=citations[num][0];
     let citation=citations[num][1];
